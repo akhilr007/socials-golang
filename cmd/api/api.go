3964be541deedd0d701350fa68c25105c779d00c
Akhil Rajan <rajanakhil07@gmail.com>
2025-10-13 23:49:38 +0530

    change folder structure

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/akhilr007/socials/internal/store"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -26,25 +25,6 @@ type dbConfig struct {
 	maxOpenConns int
 	maxIdleConns int
 	maxIdleTime  string
-}
-
-func (app *application) mount() *chi.Mux {
-	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-
-	// Set a timeout value on the request context (ctx), that will signal
-	// through ctx.Done() that the request has timed out and further
-	// processing should be stopped.
-	r.Use(middleware.Timeout(60 * time.Second))
-
-	r.Route("/v1", func(r chi.Router) {
-		r.Get("/health", app.healthCheckHandler)
-	})
-	return r
 }
 
 func (app *application) run(mux *chi.Mux) error {
---
+++ internal/handler/health.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/akhilr007/socials/internal/util"
+)
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status": "ok",
+	}
+
+	if err := util.WriteJSON(w, http.StatusOK, data); err != nil {
+		util.WriteJSONError(w, http.StatusInternalServerError, err.Error())
+	}
+}
